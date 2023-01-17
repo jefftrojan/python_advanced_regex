8857e072fe711d43306a52b82f313293a2aa3fe5
Jeff Trojan <j.dauda@alustudent.com>
2023-01-18 01:15:23 +0200

    Algo for advanced/email pattern match

---
+++ advanced/email.py
@@ -0,0 +1,13 @@
+# import regex package 
+
+# define a func that checks for pattern with one param
+
+# param should be a string obj
+
+# param is compared with string pattern
+
+# if param contains abstract format or chars, throw an exception
+# 
+# else the condition is satified, then return a message
+
+ 
